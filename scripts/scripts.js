8d4666336c3b13b31062ab7671375a3cdb80c03c
diego1871 <46872050+diego1871@users.noreply.github.com>
2021-12-22 14:18:18 -0600

    simulador con flag

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,156 +1,136 @@
-// SIMULADOR ESCUELA PORTUGUËS
-
-alert("BIENVENIDO A SIMULADOR ESCUELA DE RUSO");
-alert("Aquí encontrás cursos de todos los niveles del idioma");
-
-// variables globales
-
-let correo = "";
-
-// OBJETO constructor, CONSTRUCTOR notation: template cuando hay muchos objetos parecidos. 
-
-// function Curso(nivel, costo, descuento) {
-//   this.nivel = nivel,
-//   this.cost = costo,
-//   this.descuento = descuento,
-//     (this.calculaDescuento = function () {
-//       return this.costo * this.descuento;
-//     });
-// }
-
-
-// INSTANCIAS DE OBJETO curso que eran contruidas con objeto constructor. 
-// const curso1 = new Curso("principiante", 1000, 0.9);
-// const curso2 = new Curso("intermedio", 2000, 0.8);
-// const curso3 = new Curso("avanzado", 3000, 0.7);
-
-
-// ARRAY de objetos. 
+// ARRAY de objetos-cursos.
 
 const cursos = [
   {
-    "index": 1,
-    "nivel": "principiante",
-    "costo": 1000,
-    "descuento": .9,
+    index: 1,
+    nivel: "Principiante",
+    costo: 1000,
+    descuento: 0.9,
+    descuentoUsuario: "10%",
   },
   {
-    "index": 2,
-    "nivel": "intermedio",
-    "costo": 2000,
-    "descuento": .8,
+    index: 2,
+    nivel: "Intermedio",
+    costo: 2000,
+    descuento: 0.8,
+    descuentoUsuario: "20%",
   },
-
-]
+];
 
 cursos.push({
-  "index": 3,
-  "nivel": "avanzado",
-  "costo": 3000,
-  "descuento": .7,
-})
+  index: 3,
+  nivel: "Avanzado",
+  costo: 3000,
+  descuento: 0.7,
+  descuentoUsuario: "10%",
+});
 
-// ESCRIBIR PRIMERO Funciones globales. i.e,  cálculos que en todos los casos se van a ejecutar.
+// apuntamos elementos de html en variables mediante sus Id´s
 
-//  FUNCIÓN QUE SOLO INFORMA
-function informa(nivel, costo) {
-  alert("su curso es " + nivel);
-  alert("el precio de nivel " + nivel + " es " + costo + ".00");
-  correo = prompt("escriba su correo para comenzar proceso de incripción");
+let nombre = document.getElementById("nombre");
+let correo = document.getElementById("correo");
+let curso = document.getElementById("curso");
+let respuestaEstudiante = document.getElementById("respuestaEstudiante");
+let dineroIngresado = document.getElementById("inputDinero");
+
+// MOSTRAR INFO DE CURSOS MANIPULANDO DOM
+//primero hacemos variable que apunte a div infoCursos.
+let infoCursos = document.getElementById("infoCursos");
+//hacemos un loop que recorra cada curso de array cursos
+for (const curso of cursos) {
+  // aquí creamos un div para recibir la info de cada curso del array
+  let cajaCurso = document.createElement("div");
+
+  //aqui inyectamos codigo html en cada div que guardará info del curso.
+
+  cajaCurso.innerHTML = `<h3> ${curso.nivel}</h3>
+                      <p> precio: $ ${curso.costo} .00 </p>
+                      <p> Descuento estudiantil: ${curso.descuentoUsuario}</p>
+                      `;
+  // aquí hacemos de cada caja, un hijo del id "infoCursos" creada en HTML
+  infoCursos.appendChild(cajaCurso);
 }
 
-//funcion que solo calcula descuento
+let botonImprime = document.getElementById("imprimeInfo");
 
-function calculaDescuento(costo, descuento){
-  return costo * descuento
-}
+botonImprime.addEventListener("click", renderizeInfo);
 
+contadorA = 0; //contador de renderizeInfo
+// función que imprime inputs de usuario
+function renderizeInfo() {
+  if (contadorA == 0) {
+    //if para detener el renderizado
 
-// 2. FUNCIÓN PRINCIPAL QUE CALCULA PRECIO SEGÚN SI USUARIO ES ESTUDIANTE :
+    //hacer contenedor para recibir los datos del usuario:
+    let contenedorCurso = document.createElement("div");
+    //Definimos el innerHTML del elemento con INTERPOLACIÓN:
+    contenedorCurso.innerHTML = `
+                                <h2>DETALLE DE TU CURSO </h2>
+                                <p><strong>Nombre:</strong> ${nombre.value}</p>
+                                <p><strong>correo:</strong> ${correo.value}</p>
+                                <p><strong>Producto:</strong>Curso de Ruso ${curso.value}</p>
+                                <p><strong>Aplica descuento:</strong> ${respuestaEstudiante.value}</p>
+                                <div>
+                                <br/>
+                                <label for="">Ingresa dinero</label>
+                                </div>
+                                <br />
+                              <input type="number" required="required" id="inputDinero" />
+                              <div id="DivBotonInscripcion">
+                              <button type="button" id="btnInscripcion">Inscribete</button>
+                              </div>`;
+    //Agregamos el contenedor creado al elemento de HTML que queramos
+    document.getElementById("divDetalleCompra").appendChild(contenedorCurso);
 
-function calcularPrecioTotal(costo, descuento) {
-  let esEstudiante = prompt("Eres estudiante? si / no");
-
-
-  // PRECIO SI USUARIO ES ESTUDIANTE:*************
-
-  if (esEstudiante == "si") {
-
-    alert("tu pago TENDRÁ DESCUENTO :)"); 
-
-    const precioConDescuento = calculaDescuento(costo, descuento); //AQUI SE QUEDA !!! TypeError: curso.calculaDescuento is not a function
-
-    let pagoDescuento = prompt("ingrese la cantidad de " + precioConDescuento); 
-
-    //validación de pago para estudiantes:
-
-    while (pagoDescuento != precioConDescuento) {
-      alert("revisa que la cantidad sea exacta");
-      pagoDescuento = prompt(
-        "ingresa cantidad de " + precioConDescuento + " correctamente"
-      );
-    }
-    // mensaje  ÉXITO usuario con descuento: 
-
-    alert("Felicidades. Se ha inscrito con éxito. Enviaremos un corre a: " + correo + " con los pasos a seguir.");
-    
-    // PRECIO SI USUARIO NO ES ESTUDIANTE:*********************
-
-  } else if (esEstudiante == "no") {
-    alert("su pago es de " + costo);
-
-    let pagoNormal = prompt(
-      "ingrese la cantidad de " + costo + " para pagar curso"
-    );
-
-    // validación de pago normal:
-    while (pagoNormal != costo) {
-      alert("Revisa que la cantidad sea exacta");
-      pagoNormal = prompt("ingresa cantidad de " + costo + " correctamente");
-    }
-
-    // mensaje  ÉXITO usuario sin descuento: 
-    alert("Felicidades. Se ha inscrito con éxito. Enviaremos un corre a: " + correo + " con los pasos a seguir.");
-  }
-
-  // SI USUARIO NO INGRESA VALOR VÁLIDO SI/NO
-  else {
-    alert("Valor inválido. Comienza de nuevo. :(");
-  }
-}
-
-//inicio PROGRAMA
-
-function compraUsuario() {
-  let curso = prompt(
-    "Escribe el nivel que quieres tomar: avanzado / intermedio / principiante "
-  );
-
-  // curso avanzado 
-
-  if (curso == cursos[2].nivel) {
-    informa(cursos[2].nivel, cursos[2].costo);
-
-    calcularPrecioTotal(cursos[2].costo, cursos[2].descuento);
-
-  //curso intermedio 
-
-  } else if (curso == cursos[1].nivel) {
-    informa(cursos[1].nivel, cursos[1].costo);
-    calcularPrecioTotal(cursos[1].costo, cursos[1].descuento);
-  
-    //curso principiante 
-
-  } else if (curso == cursos[0].nivel) {
-    informa(cursos[0].nivel, cursos[0].costo);
-    calcularPrecioTotal(cursos[0].costo, cursos[0].descuento);
-  // otro
+    let varBtnInscripcion = document.getElementById("btnInscripcion");
+    varBtnInscripcion.addEventListener("click", mensajeInscripcion); // can't access property "addEventListener", varBotonInscripcion is null
+    contadorA++;
   } else {
-    alert("sorry. no tenemos ese nivel");
-    return;
+    alert("ya te inscribiste");
   }
 }
 
-//LLAMADO PARA LA EJECUCIÓN DEL PROGRAMA:
+// BOTON INSCRIPCIÓN
 
-compraUsuario();
+contadorB = 0;
+
+function mensajeInscripcion() {
+  // creo nodo donde va ir información
+
+  if (contadorB == 0) {
+    let contenedorMensaje = document.createElement("p");
+    // al nodo le inyecto texto con info de JS
+    contenedorMensaje.innerHTML = ` Te has inscrito exitosamente a tu curso de Ruso ${curso.value}`;
+    // al nodo lo hago hijo de div declarado en HTML
+    document
+      .getElementById("DivBotonInscripcion")
+      .appendChild(contenedorMensaje);
+    contadorB++;
+  } else {alert("ya te inscribiste")}
+}
+
+// FLAG DE JS- contador.
+
+// contador = 0;
+
+// function mensajeInscripcion(){
+//  if(contador == 0){
+//   let contenedorMensaje = document.createElement("p");
+//   contenedorMensaje.innerHTML = ` Te has inscrito exitosamente a tu curso de Ruso ${curso.value}`;
+//   document.getElementById("botonInscripcion").appendChild(contenedorMensaje);
+//   contador++
+//  } else {alert("etas inscrito")}
+
+// }
+
+//función prueba
+
+// function mensajeInscripcion(){
+//   console.log("funcionando")
+// }
+
+// // funcion prueba
+// function my_button_click_handler()
+// {
+// alert('Button Clcked');
+// }
